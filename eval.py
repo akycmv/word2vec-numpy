02f14b66b04d963d47fc1f2af936ca42b3e810f6
Aleksey C <a.s.chumakov@student.tue.nl>
2026-03-13 22:11:37 +0100

    w2v with reasonable results

---
+++ eval.py
@@ -0,0 +1,63 @@
+import pickle
+
+import numpy as np
+
+
+class Word2VecEval:
+    def __init__(self, W_in, words, idxs):
+        self.words = words
+        self.idxs = idxs
+
+        # normalize embeddings once
+        self.W = W_in / (np.linalg.norm(W_in, axis=1, keepdims=True) + 1e-10)
+
+    def vector(self, w):
+        return self.W[self.words[w]]
+
+    def cosine(self, w1, w2):
+        return float(self.vector(w1) @ self.vector(w2))
+
+    def most_similar(self, w, k=10):
+        v = self.vector(w)
+        sims = self.W @ v
+
+        sims[self.words[w]] = -np.inf
+        top = np.argpartition(sims, -k)[-k:]
+        top = top[np.argsort(sims[top])[::-1]]
+
+        return [(self.idxs[i], float(sims[i])) for i in top]
+
+    def analogy(self, a, b, c, k=5):
+        """
+        a - b + c
+        """
+        v = self.vector(a) - self.vector(b) + self.vector(c)
+        v /= np.linalg.norm(v) + 1e-10
+
+        sims = self.W @ v
+
+        for w in (a, b, c):
+            sims[self.words[w]] = -np.inf
+
+        top = np.argpartition(sims, -k)[-k:]
+        top = top[np.argsort(sims[top])[::-1]]
+
+        return [(self.idxs[i], float(sims[i])) for i in top]
+
+
+def show(results):
+    for w, s in results:
+        print(f"{w:<20} {s:.4f}")
+
+
+W_in = np.load("W_in.npy")
+W_out = np.load("W_out.npy")
+
+with open("vocab.pkl", "rb") as f:
+    vocab = pickle.load(f)
+    words = vocab["words"]
+    idxs = vocab["idxs"]
+    ct = vocab["ct"]
+
+w2v = Word2VecEval(W_in, words, idxs)
+show(w2v.analogy("cars", "car", "tree"))
